59a3f2d3e0509bfa2188fef6eead49b30e033573
Vishal Vaddadhi <vvaddadhi@gmail.com>
2020-08-28 00:19:00 -0700

    fixed prime function as well as pep8 and pylint formatting

--- Compute.py
+++ Compute.py
@@ -2,7 +2,7 @@ class ComputeNums:
 
     def __init__(self, number):
         self.number = number
-    
+
     def showNums(self):
         numList = []
         for i in range(1, self.number + 1):
@@ -14,20 +14,26 @@ class ComputeNums:
         for i in range(1, self.number + 1, 2):
             numList.append(i)
         return numList
-    
+
     def showEvenNums(self):
         numList = []
         for i in range(1, self.number + 1):
             if i % 2 == 0:
                 numList.append(i)
         return numList
-    
+
     def showPrimeNums(self):
-        numList = []
-        for i in range(2, self.number + 1):
-            for j in range(2, i):
-                if i % j == 0:
-                    break
-            else:
-                numList.append(i)
-        return numList
+        # list of trues to the length of inputted number
+        sieve = [True] * self.number
+
+        # this for loop only is entered for ints 9 and above
+        # if int is below 9, last return statement handles output
+        for i in range(3, int(self.number**0.5)+1, 2):
+            # iterate from 3 to sqrt of n because if you cannot find
+            # any factors less than the sqrt of n n must be prime
+            if sieve[i]:
+                # here we are setting all the multiples to false
+                sieve[i*i::2*i] = [False]*((self.number-i*i-1)//(2*i)+1)
+
+        # this return statement will return all primes
+        return [2] + [i for i in range(3, self.number, 2) if sieve[i]]
--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, jsonify, request
 from Compute import ComputeNums
 app = Flask(__name__)
 
+
 @app.route('/<int:number>/<string:option>')
 def chooseRoute(number, option):
     try:
-        if option == None:
+        if option is None:
             displayNums(number)
         elif option == 'odd':
             displayOddNums(number)
@@ -14,55 +15,70 @@ def chooseRoute(number, option):
         elif option == 'prime':
             displayPrimeNums(number)
         else:
-            return jsonify({'ERROR': 'PLEASE CHOOSE EVEN, ODD, OR PRIME AS AN OPTION OR INPUT A NUMBER AND OMIT THE OPTION PORTION'}), 400
+            return jsonify({'ERROR': '''PLEASE CHOOSE EVEN, ODD, OR PRIME AS
+            AN OPTION OR INPUT A NUMBER AND OMIT THE OPTION PORTION'''}), 400
     except Exception as e:
         return jsonify({'ERROR': str(e).upper()}), 500
 
 # route for displaying numbers from 1-N
+
+
 @app.route('/<int:number>', methods=['GET'])
 def displayNums(number):
     try:
         if number < 1:
-            return jsonify({'ERROR': 'NUMBER MUST BE GREATER THAN OR EQUAL TO 1'}), 400
-        elif isinstance(number, int) == False:
-            return jsonify({'ERROR': 'PLEASE ENTER A VALID INTEGER GREATER THAN OR EQUAL TO 1'}), 400
+            return jsonify({'ERROR': '''NUMBER MUST BE GREATER
+            THAN OR EQUAL TO 1'''}), 400
+        elif isinstance(number, int) is False:
+            return jsonify({'ERROR': '''PLEASE ENTER A VALID INTEGER GREATER
+            THAN OR EQUAL TO 1'''}), 400
         result = ComputeNums(number).showNums()
         return jsonify({'SUCCESS': result}), 200
     except Exception as e:
         return jsonify({'ERROR': str(e).upper()}), 500
 
 # route for displaying odd numbers from 1-N
+
+
 @app.route('/<int:number>/odd', methods=['GET'])
 def displayOddNums(number):
     try:
         if number < 1:
-            return jsonify({'ERROR': 'NUMBER MUST BE GREATER THAN OR EQUAL TO 1'}), 400
+            return jsonify({'ERROR': '''NUMBER MUST BE GREATER
+            THAN OR EQUAL TO 1'''}), 400
         result = ComputeNums(number).showOddNums()
         return jsonify({'SUCCESS': result}), 200
     except Exception as e:
         return jsonify({'ERROR': str(e).upper()}), 500
 
 # route for displaying even numbers from 1-N
+
+
 @app.route('/<int:number>/even', methods=['GET'])
 def displayEvenNums(number):
     try:
         if number < 1:
-            return jsonify({'ERROR': 'NUMBER MUST BE GREATER THAN OR EQUAL TO 1'}), 400
+            return jsonify({'ERROR': '''NUMBER MUST BE GREATER
+            THAN OR EQUAL TO 1'''}), 400
         result = ComputeNums(number).showEvenNums()
         return jsonify({'SUCCESS': result}), 200
     except Exception as e:
         return jsonify({'ERROR': str(e).upper()}), 500
 
 # route for displaying all prime numbers from 2 - N
+
+
 @app.route('/<int:number>/prime', methods=['GET'])
 def displayPrimeNums(number):
     try:
         if number < 2:
-            return jsonify({'ERROR': 'NUMBER MUST BE GREATER THAN OR EQUAL TO 2'}), 400
-        result = ComputeNums(number).showPrimeNums()             
+            return jsonify({'ERROR': '''NUMBER MUST BE GREATER
+            THAN OR EQUAL TO 2'''}), 400
+        result = ComputeNums(number).showPrimeNums()
         return jsonify({'SUCCESS': result}), 200
     except Exception as e:
         return jsonify({'ERROR': str(e).upper()}), 500
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
